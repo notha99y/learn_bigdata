3e02b4266cbbd1553e1f04c6854d6edd038bf97d
notha99y <renjie.tan91@gmail.com>
2020-05-04 09:13:30 +0800

    add lines to save word count results

--- SparkQL/SparkWordCount.scala
+++ SparkQL/SparkWordCount.scala
@@ -8,7 +8,9 @@ object SparkWordCount{
     def main(args:Array[String]) {
         val sc = new SparkContext("local", "Word Count", "/usr/local/spark", Nil, Map(), Map()) // the first map specifies the environment whilst the second map specifies the variables to worker nodes
 
-        val inputFilePath = ""
+        val inputFilePath = "words.txt"
         val input = sc.textFile(inputFilePath)
-        val count = input.flatMap(line -> line.split(" "))
+        val count = input.flatMap(line => line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+        println("Saving it out")
+        count.saveAsTextFile("wc_out")
 }
